44fad4a7d1776ad71e8c5b017385679803d2a677
Wafaa Shaker <wafaashaker@students.du.edu.eg>
2025-08-30 01:59:19 +0300

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:demo/feature/login/ui/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_root_checker/flutter_root_checker.dart';
 
 import 'core/networking/dio_factory.dart';
 import 'core/storage/local_storage.dart';
 
 void main() async{
+  if (FlutterRootChecker.isAndroidRoot) {
+    print("Root access detected on this Android device!");
+  } else if (FlutterRootChecker.isIosJailbreak) {
+    print("Jailbreak detected on this iOS device!");
+  } else {
+    print("Device is secure.");
+  }
+
   WidgetsFlutterBinding.ensureInitialized();
   DioFactory.init();
   await LocalStorage.init();
